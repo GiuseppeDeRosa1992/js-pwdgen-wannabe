aa8cac19ecd60ba55f286b3192b388faccfed0b5
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-16 15:42:10 +0200

    added h1 inside html and generator password with js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Chiedo all'utente il nome
+const firstName = prompt ("Inserisci il tuo Nome");
+//Chiedo all'utente il cognome
+const lastName = prompt ("Inserisci il tuo Cognome");
+//chiedo all'utente il colore preferito
+const favoriteColor = prompt ("Inserisci il tuo Colore preferito");
+// Stringa Fissa
+yearsCode = "#24"
+
+//id che devo modificare
+const idPass = "password"
+
+document.getElementById(idPass).innerHTML = firstName + lastName + favoriteColor + yearsCode
